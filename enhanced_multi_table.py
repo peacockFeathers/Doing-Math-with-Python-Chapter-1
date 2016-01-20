578b7ef9a2f308365027b8ef66502433cf5c10cb
Justin Peacock <peacockj@oregonstate.edu>
2016-01-20 10:09:35 -0800

    Can now print a multiplication table with any fixed number of rows.

--- enhanced_multi_table.py
+++ enhanced_multi_table.py
@@ -4,9 +4,11 @@ Multiplication table printer
 
 def multi_table(n):
     
-    for i in range(1, 11):
-        print('{0} x {1} = {2}'.format(a, i, a*i))
+    ROWS = 12
+    
+    for i in range(1, ROWS):
+        print('{0} x {1} = {2}'.format(n, i, n*i))
         
 if __name__ == '__main__':
-    a = input('Enter a number: ')
-    multi_table(float(a))
+    number = input('Enter a number to print its multiplication table: ')
+    multi_table(float(number))
