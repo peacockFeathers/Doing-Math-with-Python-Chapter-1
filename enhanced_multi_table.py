52a7fdfa319442c1aed40756cac384196072783e
Justin Peacock <peacockj@oregonstate.edu>
2016-01-20 10:16:52 -0800

    Completed challenge #2.

--- enhanced_multi_table.py
+++ enhanced_multi_table.py
@@ -2,13 +2,18 @@
 Multiplication table printer
 '''
 
-def multi_table(n):
+def multi_table(n, rows):
     
-    ROWS = 12
+    rows += 1 # The range function doesn't print the last iteration it reaches.
+    '''
+    For example: a 5 x 5 table would only get up to 5 x 4 = 20 without the above line.
+    '''
     
-    for i in range(1, ROWS):
+    for i in range(1, rows):
         print('{0} x {1} = {2}'.format(n, i, n*i))
         
 if __name__ == '__main__':
     number = input('Enter a number to print its multiplication table: ')
-    multi_table(float(number))
+    rows = input('Enter the number of rows the multiplication table should be: ')
+    
+    multi_table(float(number), int(rows))
