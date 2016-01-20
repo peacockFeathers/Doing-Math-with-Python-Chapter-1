6931869a18009afc714ec76e1bce99b89f2aaa6e
Justin Peacock <peacockj@oregonstate.edu>
2016-01-20 11:11:01 -0800

    Added plan for the next commit.

--- bit-byte_converter.py
+++ bit-byte_converter.py
@@ -6,7 +6,7 @@ def bits_to_bytes():
 
     bits = input('Enter the number of bits, so I can convert them to bytes: ')
     bits = int(bits)
-    print('{0} bits equals {1} bytes.'.format(bits, int(bits/8)))
+    print('{0} bits equals {1} bytes.'.format(bits, int(bits/8))) # This currently cuts off bits. It should use modulus to check for a remainder.
     
 if __name__ == '__main__':
 
