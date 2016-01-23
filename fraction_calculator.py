3ed432e8c817e4b8863dfd7cc83ba03c665c399c
Justin Peacock <peacockj@oregonstate.edu>
2016-01-23 10:25:58 -0800

    Added functions
    Subtraction, Division, Multiplication

--- fraction_calculator.py
+++ fraction_calculator.py
@@ -6,9 +6,24 @@ from fractions import Fraction
 def add(a, b):
     print('Result of Addition: {0}'.format(a+b))
     
+def sub(a, b):
+    print('Result of Subtraction: {0}'.format(a-b))
+    
+def div(a, b):
+    print('Result of Division: {0}'.format(a/b))
+    
+def mul(a, b):
+    print('Result of Multiplication: {0}'.format(a*b))
+    
 if __name__ == '__main__':
     a = Fraction(input('Enter first fraction: '))
     b = Fraction(input('Enter second fraction: '))
     op = input('Operation to perform - Add, Subtract, Divide, Multiply: ')
     if op == 'Add':
         add(a, b)
+    if op == 'Subtract':
+        sub(a, b)
+    if op == 'Divide':
+        div(a, b)
+    if op == 'Multiply':
+        mul(a, b)
