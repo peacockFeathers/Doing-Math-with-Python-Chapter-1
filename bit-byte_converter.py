3c27370b51ae29a5b502566937c548d6665f8106
Justin Peacock <peacockj@oregonstate.edu>
2016-01-23 10:05:04 -0800

    Prints bits as change.

--- bit-byte_converter.py
+++ bit-byte_converter.py
@@ -6,7 +6,10 @@ def bits_to_bytes():
 
     bits = input('Enter the number of bits, so I can convert them to bytes: ')
     bits = int(bits)
-    print('{0} bits equals {1} bytes.'.format(bits, int(bits/8))) # This currently cuts off bits. It should use modulus to check for a remainder.
+    print('{0} bits equals {1} bytes, {2} bits.'.format(bits, int(bits/8), bits%8)) 
+    # Roadmap: 
+    # * Convert between bps and kbps, mbps
+    # * Singular/plural bit/bits; byte/byteså
     
 if __name__ == '__main__':
 
